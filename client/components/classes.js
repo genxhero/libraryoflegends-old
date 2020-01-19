e2c3d68201456084d54c1696145a495309f2ad30
genxhero <genxhero@users.noreply.github.com>
2020-01-19 11:55:31 -0800

    classes constants

---
+++ client/components/classes.js
@@ -0,0 +1,86 @@
+const Alchemist = {
+    name: "alchemist",
+    keyAbility: "intelligence",
+    description: "Mutagens...potions...bombs...let's face it, you're basically Geralt of Rivia."
+}
+
+const Barbarian = {
+    name: "barbarian",
+    keyAbility: "strength",
+    description: "You are a warrior driven by rage, and you know what is best in life.  You join the ranks of such legendary wild men as Enkidu, Conan, and Wulfgar."
+}
+
+const Bard = {
+    name: "bard",
+    keyAbility: "charisma",
+    description: "You inspire courage everywhere you go with your poetry and song. The minstrel of Sir Robin, William Shakespeare, and Makoto Naegi"
+}
+
+const Cleric = {
+    name: "cleric",
+    keyAbility: "wisdom",
+    description: "You are a conduit of divine power.  At the behest of your chosen deity you embark upon a quest.  Insert example of famous holy men here."
+}
+
+const Druid = {
+    name: "druid",
+    keyAbility: "wisdom",
+    description: "Nature is your bread and butter, and you are an expert baker."
+}
+
+const Fighter = {
+     name: "fighter",
+     keyAbility: ["strength", "dexterity"],
+     description: "You are a fighter; that is what you do. You fight. You walk the path of Musashi, Achilles, Gimli, and many more."
+}
+
+const Monk = {
+    name: "monk",
+    keyAbility: ["dexterity", "strength"],
+    description: "A martial artist dedicated to self-perfection.  You rank among such heroes as Bruce Lee, Chuck Norris, and the Karate Kid"
+}
+
+const Paladin = {
+    name: "paladin",
+    keyAbility: "strength",
+    description: "A holy knight errant on a divine quest, tasked with helping the weak and protecting the innocent along the way."
+}
+
+const Ranger = {
+    name: "ranger",
+    keyAbility: "dexterity",
+    description: "A warrior more at home in the wilds than in the cities.  You join the ranks of such heroes as Aragorn and Drizzt Do'Urden"
+}
+
+const Rogue = {
+    name: "rogue",
+    keyAbility: ["strength", "dexterity"],
+    description: "Full of cunning and guile, you follow in the footsteps of such great characters as Han Solo, Locke, and Robin Hood"
+}
+
+const Sorcerer = {
+    name: "sorcerer",
+    keyAbility: "charisma",
+    description: "The power of your blood has gifted you with a mastery of magic that wizards can only dream of."
+}
+
+const Wizard = {
+    name: 'wizard',
+    keyAbility: "intelligence",
+    description: "The boundless powers of the arcane arts are your bread and butter.  The wizard has long been an advisor to heroes and kings, and includes such names as Gandalf, Merlin, and Elminster"
+}
+
+export default [
+        Alchemist, 
+        Barbarian, 
+        Bard, 
+        Cleric, 
+        Druid, 
+        Fighter, 
+        Monk, 
+        Paladin, 
+        Ranger, 
+        Rogue, 
+        Sorcerer, 
+        Wizard
+    ];
